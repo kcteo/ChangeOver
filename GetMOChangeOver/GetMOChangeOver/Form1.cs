446bc304cf4017de33a4605db6e100cafcbbac54
kcteo <eugeneteokc@yahoo.com>
2015-05-10 20:44:54 +0800

    version 1.0

---
+++ GetMOChangeOver/GetMOChangeOver/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetMOChangeOver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string address = "net.tcp://1202sgn157:9500/Siemens.Siplace.Oib.SiplacePro";
+            //this.Cursor = Cursors.WaitCursor;
+#region connection to SiplacePro
+            try
+            {
+                
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+
+#endregion 
+        }
+    }
+}
